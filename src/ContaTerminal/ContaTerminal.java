95713069d611d8a2d15294751797d2932425d4a1
Victor <victorhrc372@gmail.com>
2023-03-14 13:30:37 -0300

    Primeiro Commit

---
+++ src/ContaTerminal/ContaTerminal.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package ContaTerminal;
+
+/**
+ * @author Victor
+ *
+ */
+public class ContaTerminal {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		String agencia, nomeCliente;
+		int numero;
+		double saldo;
+		
+		System.out.println("Por favor, digite o número da Agência !" + agencia );
+		
+	}
+
+}
